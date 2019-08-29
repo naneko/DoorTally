c69da1c5d801b6eb93259a435c696a5454d80ed3
Ben Saltz <bensaltz@gmail.com>
2019-08-29 00:37:23 -0600

    Switched to Socket.IO instead of AJAX for data transfer

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import threading
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, make_response
 from flask_seasurf import SeaSurf
+from flask_socketio import SocketIO
 
 APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 app.config.update(
     SECRET_KEY=os.environ.get('SECRET_KEY'),
@@ -18,32 +20,13 @@ app.config.update(
 csrf = SeaSurf(app)
 
 
-class Counter(threading.Thread):
+# TODO: Add data persistence across restart
+class Counter():
     def __init__(self, value):
-        threading.Thread.__init__(self)
-        self.e = threading.Event()
         self.value = value
-
-    def set_value(self, value):
-        self.value = value
-        self.e.set()
-
-    def get_value(self):
-        self.e.clear()
-        self.e.wait()
-        return str(self.value)
 
 
 counter = Counter(0)
-counter.start()
-
-
-def no_cache(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
 
 
 @app.route('/')
@@ -51,38 +34,53 @@ def home():
     return render_template('default.html')
 
 
-# TODO: Try to find a way to redirect when API pages are accessed directly (Though they wouldn't do anything anyways)
-@app.route('/api/element/login')
+@socketio.on_error()        # Handles the default namespace
+def error_handler(e):
+    print(e)
+
+
+@socketio.on('element')
+def send_element(name):
+    switch = {
+        "login": api_element_login,
+        "app": api_element_app
+    }
+    element = switch.get(name, lambda: "404 Error: Element not found")
+    return element()
+
+
 def api_element_login():
-    # TODO: This should check to make sure the user is in an active session and return an error if the user is not in an active session
-    r = make_response(render_template('login.html'))
-    return no_cache(r)
+    return render_template('login.html')
 
 
-@app.route('/api/element/login/action/login', methods=['POST'])
-def api_action_login():
-    # TODO: Check user pin and create new session if correct (this session should be sent back to the client for storage)
-    # TODO: If pin is empty but user has valid session, grant login
-    data = request.data
+def api_element_app():
+    return render_template('app.html')
+
+
+@socketio.on('action')
+def do_action(name, data):
+    switch = {
+        "login": api_action_login,
+        "counter": api_action_counter
+    }
+    action = switch.get(name, lambda: "400 Error: Action not found")
+    return action(data)
+
+
+# TODO: Actually validate login and session
+def api_action_login(data):
     return "ok"
 
 
-@app.route('/api/element/app')
-def api_element_app():
-    r = make_response(render_template('app.html'))
-    return no_cache(r)
-
-
-@app.route('/api/element/app/listener/count')
-def api_listener_count():
-    return counter.get_value()
-
-
-@app.route('/api/element/app/action/count')
-def api_action_count():
-    counter.set_value(1000)
+# TODO: Validate user session first
+def api_action_counter(data):
+    if data == "add":
+        counter.value += 1
+    if data == "subtract":
+        counter.value -= 1
+    socketio.emit('counter', counter.value, broadcast=True)
     return "ok"
 
 
 if __name__ == '__main__':
-    app.run(app)
+    socketio.run(app)
--- static/app.js
+++ static/app.js
@@ -1,5 +1,10 @@
 let page;
 let session;
+let debug = true;
+
+const socket = io();
+
+// TODO: Add comments
 
 /*
     Quick rundown of terminology:
@@ -8,101 +13,45 @@ let session;
  */
 
 /*
-    On-load
- */
-$(function() {
-    // Set page to content div
-    page = $("#content");
-
-    // Add CSRF token to AJAX header from the hidden input field on the page
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            let csrftoken = $("#_csrf_token").val();
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    });
-
-    // Debug Hashes
-    // If you are fancy and think you can use the debug features to l33t h4x0r the system, the server still validates your user session so things won't work
-    switch(window.location.hash) {
-        case "#debug-app":
-            loadApp();
-            break;
-        default:
-            loadLogin();
-    }
-});
-
-/*
     Loads element from server and shows error on fail
 */
-function loadElement(url, callback) {
+function loadElement(name, callback) {
     page.empty();
-    // Try loading the page
-    page.load(url, function( response, status, xhr ) {
-      if ( status === "error" ) {
-          alert("Sorry but there was an error: " + xhr.status + " " + xhr.statusText );
-          throw "Failed to load element " + url + ": " + xhr.status + " " + xhr.statusText;
-      }
-      // Check if the callback is indeed a function, then run it
-      else if($.isFunction(callback)) {
+    // Load the page
+    socket.emit('element', name, (data) => {
+        page.html(data);
+        if($.isFunction(callback)) {
           callback()
-      }
-      else if(callback !== undefined) {
-          throw "Callback is not a function";
-      }
-      console.log("Element Loaded: " + url);
+        }
+        else if(callback !== undefined) {
+            alert("An error occurred: The app may no longer function without reloading the page");
+            throw "Callback is not a function";
+        }
+        if(debug) console.log("Element Loaded: " + name);
     });
 }
 
 /*
     Executes an action on the server
  */
-function action(url, data, callback) {
+function action(name, data, callback) {
     // Check if callback is a function
-    if($.isFunction(callback) && typeof callback !== undefined) {
-        // Make AJAX call
-        $.ajax({
-            method: "POST",
-            url: url,
-            data: data,
-            cache: false,
-        })
-            .done(function(response){ // Execute callback and pass response data as attribute
-                console.log("Action Executed: " + url);
-                callback(response)
-            })
-            .fail(function (jqXHR, textStatus) {
-                console.error("Action Failed to Execute: " + url);
-                connectionFailed(jqXHR, textStatus)
-            })
-    }
-    else if(callback !== undefined) {
-        throw "Callback is not a function";
-    }
-}
-
-/*
-    Creates a new listener that awaits a response from the server and executes a callback on success
- */
-function listener(url, callback) {
-    $.ajax({
-        url: url,
-        type: 'GET',
-        cache: false,
-    })
-        .done(function(response){ // Execute callback and pass response data as attribute
-            callback(response)
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            console.error("Listener Failed to Connect: " + url);
-            connectionFailed(jqXHR, textStatus, errorThrown)
-        })
+    socket.emit('action', name, data, (data) => {
+        if ($.isFunction(callback)) {
+            callback(data)
+        }
+        else if (callback !== undefined) {
+            alert("An error occurred while performing the action " + name);
+            throw "Callback is not a function";
+        }
+    });
+    if(debug) console.log("Action Executed: " + name + " | " + data);
 }
 
 /*
     Connection Failed handler
  */
+// TODO: Periodically test/detect disconnect
 function connectionFailed(jqXHR, textStatus, errorThrown) {
     console.error("Connection failed! " + textStatus + " " + errorThrown);
     alert("Sorry but it seems you have been disconnected")
@@ -115,8 +64,8 @@ function connectionFailed(jqXHR, textStatus, errorThrown) {
     Loads the login element
  */
 function loadLogin() {
-    console.log("Loading login...");
-    loadElement("/api/element/login", loginOperator)
+    if(debug) console.log("Loading login...");
+    loadElement("login", loginOperator)
 }
 
 /*
@@ -127,9 +76,9 @@ function loginOperator() {
     let buttonVal = "";
     $(".numpad-button").on('click',function() { // On-click for a numpad button
         buttonVal = $(this).attr('data-value'); // Get the button value
-        $(this).fadeTo(0, 0.3, function() { $(this).fadeTo(500, 1.0); }); // Animate the button
+        $(this).stop().fadeTo(0, 0.3, function() { $(this).fadeTo(500, 1.0); }); // Animate the button
         if(buttonVal === "enter") { // If the button is "enter"
-            action("/api/element/login/action/login", { pin: numpadVal, session: session }, function(response) { // Execute login action
+            action("login", { pin: numpadVal, session: session }, function(response) { // Execute login action
                 // Login callback
                 if(response === "ok") {
                     loadApp()
@@ -152,20 +101,46 @@ function loginOperator() {
     Loads the app element
  */
 function loadApp() {
-    console.log("Loading app...");
-    loadElement("/api/element/app", appOperator);
+    if(debug) console.log("Loading app...");
+    loadElement("app", appOperator);
 }
 
 function appOperator() {
-    function counterListener(response) {
-        $(".counter").text(response);
-        listener("/api/element/app/listener/count", counterListener);
-    }
-    counterListener(0);
+    // TODO: Add 1s timeout function to detect disconnect
+    socket.on('counter', (data) => {
+        $(".counter").text(data).stop().fadeTo(0, 0.3, function() { $(this).fadeTo(500, 1.0); });
+    });
+    action("counter");
     $(".up").on("click", function() {
-        $(this).fadeTo(0, 0.3, function() { $(this).fadeTo(500, 1.0); });
+        $(this).stop().fadeTo(0, 0.3, function() { $(this).fadeTo(500, 1.0); });
+        action("counter", "add");
     });
     $(".down").on("click", function() {
-        $(this).fadeTo(0, 0.3, function() { $(this).fadeTo(500, 1.0); });
+        $(this).stop().fadeTo(0, 0.3, function() { $(this).fadeTo(500, 1.0); });
+        action("counter", "subtract");
     });
 }
+
+//-----
+
+/*
+    On-load
+ */
+$(function() {
+    // Set page to content div
+    page = $("#content");
+
+    // Debug Hashes
+    // If you are fancy and think you can use the debug features to l33t h4x0r the system, the server still validates your user session so things won't work
+    switch(window.location.hash) {
+        case "#debug":
+            loadLogin();
+            break;
+        case "#debug-app":
+            loadApp();
+            break;
+        default:
+            debug = false;
+            loadLogin();
+    }
+});
